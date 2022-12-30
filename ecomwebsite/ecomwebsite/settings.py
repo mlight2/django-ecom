a0f37c90504cdf469d2e3f38e92e05affaf2e4dd
mlight2 <shubhamlight2@gmail.com>
2022-12-30 19:49:54 +0530

    removed

--- ecomwebsite/ecomwebsite/settings.py
+++ ecomwebsite/ecomwebsite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4&bo+r7c8_*1@7gxi7!&j_6*65s-i=1ex$9b6vu9319^kwn4b2'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
